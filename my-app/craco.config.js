2e216f84f75ee050cf07e9f4285343ce4863d422
wenquansun <604164227@qq.com>
2023-03-20 14:44:16 +0800

    add my-app

---
+++ my-app/craco.config.js
@@ -0,0 +1,13 @@
+// const path = require('path');
+module.exports = {
+  devServer: {
+    port: 3301,
+    static: ['./public', '../build/node_modules'],
+  },
+  webpack: {
+    externals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+    },
+  },
+};
